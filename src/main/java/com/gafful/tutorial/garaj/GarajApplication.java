1188a6751ec6855da747a6881718bfb794d8e962
Godfred Afful <affulgodfred@gmail.com>
2021-10-20 23:44:51 +0000

    Add bare bones spring application.

---
+++ src/main/java/com/gafful/tutorial/garaj/GarajApplication.java
@@ -0,0 +1,13 @@
+package com.gafful.tutorial.garaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarajApplication.class, args);
+	}
+
+}
